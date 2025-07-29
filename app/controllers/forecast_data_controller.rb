88eff4bd6f02d7d7ee1a3d9ed34086323b3f2772
Erick Trevino <erick.trevino@cloudlite.io>
2025-07-29 18:59:44 -0500

    Add alerts on submission fail

--- app/controllers/forecast_data_controller.rb
+++ app/controllers/forecast_data_controller.rb
@@ -5,17 +5,28 @@ class ForecastDataController < ApplicationController
   def create
     address = params[:address]
     zip = address.split(" ")[-1] # this is pretty flaky, ideally would like to implement an address checker and extract the zipcode if the address is valid
-    @cached = false
 
     @current_conditions = Rails.cache.read(zip)
 
     if @current_conditions
       @cached = true
     else
-      @current_conditions = FetchAddressCurrentConditionsService.new(address).call
-      Rails.cache.write(zip, @current_conditions, expires_in: 30.minutes)
+      begin
+        @current_conditions = FetchAddressCurrentConditionsService.new(address).call
+        Rails.cache.write(zip, @current_conditions, expires_in: 30.minutes)
+      rescue StandardError => e
+        @alerts = [{ type: 'error', message: e.message }]
+        invalid_address_response
+      end
     end
+  end
 
-    turbo_stream
+  private
+
+  def invalid_address_response
+    respond_to do |format|
+      format.turbo_stream { render :invalid_address }
+      format.html { redirect_to :index }
+    end
   end
 end
