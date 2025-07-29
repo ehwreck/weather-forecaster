148f9605e1785ea57853318bc0f45539b15d94e5
Erick Trevino <erick.trevino@cloudlite.io>
2025-07-29 16:19:58 -0500

    Fix linter issues

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  resources "forecast_data", only: [:index, :create]
+  resources "forecast_data", only: [ :index, :create ]
 
   root "forecast_data#index"
 end
