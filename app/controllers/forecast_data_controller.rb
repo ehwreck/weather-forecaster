5149012d6420d570bc641b7ce17ef2f1c11f0ef6
Erick Trevino <erick.trevino@cloudlite.io>
2025-07-29 16:18:02 -0500

    Add 30 minute caching on zip code

--- app/controllers/forecast_data_controller.rb
+++ app/controllers/forecast_data_controller.rb
@@ -4,8 +4,18 @@ class ForecastDataController < ApplicationController
 
   def create
     address = params[:address]
-    @current_conditions = FetchAddressCurrentConditionsService.new(address).call
-    
+    zip = address.split(" ")[-1] # this is pretty flaky, ideally would like to implement an address checker and extract the zipcode if the address is valid
+    @cached = false
+
+    @current_conditions = Rails.cache.read(zip)
+
+    if @current_conditions
+      @cached = true
+    else
+      @current_conditions = FetchAddressCurrentConditionsService.new(address).call
+      Rails.cache.write(zip, @current_conditions, expires_in: 30.minutes)
+    end
+
     turbo_stream
   end
 end
