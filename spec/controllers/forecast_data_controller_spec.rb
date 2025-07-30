ec4a1a612a3d0f0bacf27ffb970083ec4b58d80c
Erick Trevino <erick.trevino@cloudlite.io>
2025-07-30 13:50:22 -0500

    Add cassette usage to controller specs

--- spec/controllers/forecast_data_controller_spec.rb
+++ spec/controllers/forecast_data_controller_spec.rb
@@ -12,17 +12,29 @@ RSpec.describe ForecastDataController, type: :controller do
 
   describe "POST /forecast_data" do
     it 'returns 200 if request successfull and responds with turbo_stream create' do
-      post :create, params: { address: "1600 Pennsylvania Ave NW, Washington, DC 20500" }, format: :turbo_stream
+      response = with_google_maps_cassettes do
+        post :create, params: { address: "1600 Pennsylvania Ave NW, Washington, DC 20500" }, format: :turbo_stream
+      end
 
       expect(response).to have_http_status(200)
       expect(response.content_type).to eq("text/vnd.turbo-stream.html; charset=utf-8")
     end
 
-    it 'returns 400 if the request is not successfull and responds with turbo_stream invalid_address' do
-      post :create, params: { address: "some fake address" }, format: :turbo_stream
+    it 'returns 422 if the request is not successfull and responds with turbo_stream invalid_address' do
+      response = with_google_maps_cassettes do
+        post :create, params: { address: "some fake address" }, format: :turbo_stream
+      end
 
       expect(response).to have_http_status(422)
       expect(response.content_type).to eq("text/vnd.turbo-stream.html; charset=utf-8")
+    end
+  end
+
+  def with_google_maps_cassettes
+    VCR.use_cassette("successfully_geocoded_address") do
+      VCR.use_cassette("successfully_fetch_current_conditions") do
+        yield
+      end
     end
   end
 end
